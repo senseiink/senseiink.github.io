c179f9b39db5cf476dcce3acf8a45a2659d3aebf
Abani Ikenna <ikennaabani080@gmail.com>
2026-08-14 04:05:58 +0100

    Add personal portfolio site

---
+++ index.js
@@ -0,0 +1,54 @@
+/* -----------------------------------------
+  Have focus outline only for keyboard users 
+ ---------------------------------------- */
+
+const handleFirstTab = (e) => {
+  if(e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing')
+
+    window.removeEventListener('keydown', handleFirstTab)
+    window.addEventListener('mousedown', handleMouseDownOnce)
+  }
+
+}
+
+const handleMouseDownOnce = () => {
+  document.body.classList.remove('user-is-tabbing')
+
+  window.removeEventListener('mousedown', handleMouseDownOnce)
+  window.addEventListener('keydown', handleFirstTab)
+}
+
+window.addEventListener('keydown', handleFirstTab)
+
+const backToTopButton = document.querySelector(".back-to-top");
+let isBackToTopRendered = false;
+
+let alterStyles = (isBackToTopRendered) => {
+  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+  backToTopButton.style.transform = isBackToTopRendered
+    ? "scale(1)"
+    : "scale(0)";
+};
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 700) {
+    isBackToTopRendered = true;
+    alterStyles(isBackToTopRendered);
+  } else {
+    isBackToTopRendered = false;
+    alterStyles(isBackToTopRendered);
+  }
+});
+
+const emailRevealButton = document.querySelector("#email-reveal");
+if (emailRevealButton) {
+  const user = ["ikennaabani080"];
+  const domain = ["gmail", "com"];
+  const email = user.join("") + "@" + domain[0] + "." + domain[1];
+
+  emailRevealButton.addEventListener("click", () => {
+    window.location.href = "mailto:" + email;
+  });
+}
